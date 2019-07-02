71192198019c8cd1ac699d8a7bd73cf29a2ab51f
colt <colten@utexas.edu>
2019-07-02 00:31:42 -0500

    Updates

---
+++ precache-manifest.12a15b87d4ce629012df773dde50600d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5817d5e2ad4702724414d63f1cf7ac2b",
+    "url": "/js-tracker/index.html"
+  },
+  {
+    "revision": "61ea02978540d7de85ef",
+    "url": "/js-tracker/static/css/main.88cac6ac.chunk.css"
+  },
+  {
+    "revision": "bcc37ca51fc762185a5c",
+    "url": "/js-tracker/static/js/2.d55c4a5b.chunk.js"
+  },
+  {
+    "revision": "61ea02978540d7de85ef",
+    "url": "/js-tracker/static/js/main.1d830996.chunk.js"
+  },
+  {
+    "revision": "394430cd35fa351a1f33",
+    "url": "/js-tracker/static/js/runtime~main.aedf3b6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcdf8730d73c813b01f2dc350236367f.js"
+  "/js-tracker/precache-manifest.12a15b87d4ce629012df773dde50600d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/js-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
